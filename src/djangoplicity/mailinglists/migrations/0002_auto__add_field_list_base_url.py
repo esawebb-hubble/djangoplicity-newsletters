12ead0c944478b2038accc235ffad9dbfd6caf58
lnielsen_eso <devnull@localhost>
2011-09-30 12:22:53 +0200

    - Fixed migration dependency problem, preventing list.base_url to be properly created.

--- src/djangoplicity/mailinglists/migrations/0002_auto__add_field_list_base_url.py
+++ src/djangoplicity/mailinglists/migrations/0002_auto__add_field_list_base_url.py
@@ -5,98 +5,101 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 class Migration(SchemaMigration):
+	depends_on = (
+		( "newsletters", "0004_auto__del_list__del_bademailaddress__del_mailchimplisttoken__del_mailc" ),
+	)
 
-    def forwards(self, orm):
-        
-        # Adding field 'List.base_url'
-        db.add_column('mailinglists_list', 'base_url', self.gf('django.db.models.fields.CharField')(default='', max_length=255), keep_default=False)
-
-
-    def backwards(self, orm):
-        
-        # Deleting field 'List.base_url'
-        db.delete_column('mailinglists_list', 'base_url')
+	def forwards(self, orm):
+		
+		# Adding field 'List.base_url'
+		db.add_column('mailinglists_list', 'base_url', self.gf('django.db.models.fields.CharField')(default='', max_length=255), keep_default=False)
 
 
-    models = {
-        'mailinglists.bademailaddress': {
-            'Meta': {'ordering': "('email',)", 'object_name': 'BadEmailAddress'},
-            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
-        },
-        'mailinglists.list': {
-            'Meta': {'ordering': "('name',)", 'object_name': 'List'},
-            'base_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_sync': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
-            'password': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
-            'subscribers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.Subscriber']", 'symmetrical': 'False', 'through': "orm['mailinglists.Subscription']", 'blank': 'True'})
-        },
-        'mailinglists.mailchimplist': {
-            'Meta': {'ordering': "('name',)", 'object_name': 'MailChimpList'},
-            'api_key': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
-            'avg_sub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'avg_unsub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'cleaned_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'cleaned_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'click_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'connected': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'default_from_email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'default_from_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'default_language': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
-            'default_subject': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'email_type_option': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'last_sync': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'list_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
-            'list_rating': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'member_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'member_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
-            'open_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'sources': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.List']", 'through': "orm['mailinglists.MailChimpSourceList']", 'symmetrical': 'False'}),
-            'subscriber_excludes': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.Subscriber']", 'through': "orm['mailinglists.MailChimpSubscriberExclude']", 'symmetrical': 'False'}),
-            'synchronize': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'target_sub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'unsubscribe_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'unsubscribe_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'use_awesomebar': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'web_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
-        },
-        'mailinglists.mailchimplisttoken': {
-            'Meta': {'object_name': 'MailChimpListToken'},
-            'expired': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"}),
-            'token': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '56'}),
-            'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36'})
-        },
-        'mailinglists.mailchimpsourcelist': {
-            'Meta': {'unique_together': "(('mailchimplist', 'list'),)", 'object_name': 'MailChimpSourceList'},
-            'default': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.List']"}),
-            'mailchimplist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"})
-        },
-        'mailinglists.mailchimpsubscriberexclude': {
-            'Meta': {'unique_together': "(('mailchimplist', 'subscriber'),)", 'object_name': 'MailChimpSubscriberExclude'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'mailchimplist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"}),
-            'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.Subscriber']"})
-        },
-        'mailinglists.subscriber': {
-            'Meta': {'ordering': "('email',)", 'object_name': 'Subscriber'},
-            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
-        },
-        'mailinglists.subscription': {
-            'Meta': {'ordering': "('subscriber__email',)", 'unique_together': "(('subscriber', 'list'),)", 'object_name': 'Subscription'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.List']"}),
-            'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.Subscriber']"})
-        }
-    }
+	def backwards(self, orm):
+		
+		# Deleting field 'List.base_url'
+		db.delete_column('mailinglists_list', 'base_url')
 
-    complete_apps = ['mailinglists']
+
+	models = {
+		'mailinglists.bademailaddress': {
+			'Meta': {'ordering': "('email',)", 'object_name': 'BadEmailAddress'},
+			'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+		},
+		'mailinglists.list': {
+			'Meta': {'ordering': "('name',)", 'object_name': 'List'},
+			'base_url': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'last_sync': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+			'name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+			'password': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+			'subscribers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.Subscriber']", 'symmetrical': 'False', 'through': "orm['mailinglists.Subscription']", 'blank': 'True'})
+		},
+		'mailinglists.mailchimplist': {
+			'Meta': {'ordering': "('name',)", 'object_name': 'MailChimpList'},
+			'api_key': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '255'}),
+			'avg_sub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'avg_unsub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'cleaned_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'cleaned_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'click_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'connected': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'default_from_email': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+			'default_from_name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+			'default_language': ('django.db.models.fields.CharField', [], {'max_length': '10', 'blank': 'True'}),
+			'default_subject': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+			'email_type_option': ('django.db.models.fields.CharField', [], {'max_length': '50', 'blank': 'True'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'last_sync': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+			'list_id': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+			'list_rating': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'member_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'member_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'name': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'}),
+			'open_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'sources': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.List']", 'through': "orm['mailinglists.MailChimpSourceList']", 'symmetrical': 'False'}),
+			'subscriber_excludes': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['mailinglists.Subscriber']", 'through': "orm['mailinglists.MailChimpSubscriberExclude']", 'symmetrical': 'False'}),
+			'synchronize': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'target_sub_rate': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'unsubscribe_count': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'unsubscribe_count_since_send': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+			'use_awesomebar': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'web_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'blank': 'True'})
+		},
+		'mailinglists.mailchimplisttoken': {
+			'Meta': {'object_name': 'MailChimpListToken'},
+			'expired': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"}),
+			'token': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '56'}),
+			'uuid': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '36'})
+		},
+		'mailinglists.mailchimpsourcelist': {
+			'Meta': {'unique_together': "(('mailchimplist', 'list'),)", 'object_name': 'MailChimpSourceList'},
+			'default': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.List']"}),
+			'mailchimplist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"})
+		},
+		'mailinglists.mailchimpsubscriberexclude': {
+			'Meta': {'unique_together': "(('mailchimplist', 'subscriber'),)", 'object_name': 'MailChimpSubscriberExclude'},
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'mailchimplist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.MailChimpList']"}),
+			'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.Subscriber']"})
+		},
+		'mailinglists.subscriber': {
+			'Meta': {'ordering': "('email',)", 'object_name': 'Subscriber'},
+			'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '75'}),
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+		},
+		'mailinglists.subscription': {
+			'Meta': {'ordering': "('subscriber__email',)", 'unique_together': "(('subscriber', 'list'),)", 'object_name': 'Subscription'},
+			'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+			'list': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.List']"}),
+			'subscriber': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['mailinglists.Subscriber']"})
+		}
+	}
+
+	complete_apps = ['mailinglists']
