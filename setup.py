aab08f2772c43754e39ebd7302942f873263ec4c
Mathias Andre <mandre@eso.org>
2014-04-14 11:25:35 +0200

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -38,14 +38,10 @@ setup(
 	version = '0.1.0',
 	packages = find_packages('src'),
 	package_dir = { '': 'src' },
+    install_requires = ['setuptools'],
 	include_package_data = True,
 	zip_safe = False,
 	namespace_packages = ['djangoplicity'],
-	#entry_points = {
-	#	'console_scripts': [
-	#			'somescriptname = djangoplicity.newsletters.script:main',
-	#		],		
-	#	},
 	
 	# metadata for upload to PyPI
 	author = 'European Southern Observatory',
